340c55b1572d567b506ebb95efea35ae6e1e86c6
Mylene Pont <pont.mylene@gmail.com>
2021-07-08 12:22:58 +0900

    seed work now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
 require 'faker'
 
-Restaurant.destroy_all 
-
+puts "Seed restaurants..."
 5.times do
   Restaurant.create(
     name: Faker::Name.name,
 	  address: Faker::Address.street_address,
     phone_number: Faker::PhoneNumber.cell_phone,
     category: (["Chinese", "Italian", "Japanese", "French", "Belgian"].sample)
-)
+  )
 end
