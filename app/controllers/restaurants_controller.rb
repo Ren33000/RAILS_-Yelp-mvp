c40788ae47d06c48130982a97f3bc05165d10cd2
Mylene Pont <pont.mylene@gmail.com>
2021-07-08 19:45:25 +0900

    finished the challenge, can do more on the front end later

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,7 +4,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-	@restaurants = Restaurant.find(params[:id])
+	@restaurant = Restaurant.find(params[:id])
   end
 
   def new 
@@ -13,24 +13,16 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant) 
-  end
-
-# Create with if /else condition inside
-  def create
-    @pet = Pet.new(pet_params)
-    if @pet.save
-      redirect_to pet_path(@pet)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
     else
       render 'new'
     end
   end
 
-
   private
-  
+
   def restaurant_params #to protect from hacking in post, checking the params sent
-    params.require(:restaurant).permit(:name, :address, :rating)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
